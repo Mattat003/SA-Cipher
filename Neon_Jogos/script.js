53908da2afe67e0b1608b77875e79d0d025b2b96
NeonGustavo <Neon.top75@gmail.com>
2025-04-04 14:28:12 -0300

    Script.js
    Script Javascript

--- Neon_Jogos/script.js
+++ Neon_Jogos/script.js
@@ -1,9 +1,28 @@
 document.getElementById("addJogo").addEventListener("click", function() {
     let novoJogo = prompt("Digite o nome do novo jogo:");
+    let urlImagem = prompt("Cole a URL da imagem do jogo (ou deixe em branco para nenhum)");
+
     if (novoJogo) {
         let lista = document.getElementById("listaJogos");
+
         let novoItem = document.createElement("li");
-        novoItem.textContent = novoJogo;
+
+        let nomeJogo = document.createElement("p");
+        nomeJogo.textContent = novoJogo;
+
+        novoItem.appendChild(nomeJogo);
+
+        if (urlImagem) {
+            let imagem = document.createElement("img");
+            imagem.src = urlImagem;
+            imagem.alt = novoJogo;
+            imagem.style.width = "100px";
+            imagem.style.height = "100px";
+            imagem.style.borderRadius = "5px";
+            imagem.style.marginTop = "5px";
+            novoItem.appendChild(imagem);
+        }
+
         lista.appendChild(novoItem);
     }
 });
