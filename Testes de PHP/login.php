85b034b0bdbd512a7298df908d8114b8c38b57cb
MatheusLeal <contatomatheusleal15@gmail.com>
2025-04-28 15:31:34 -0300

    Testes

---
+++ Testes de PHP/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/Login.css">
+</head>
+<body>
+     <div class="login-container">
+        <h2>Login</h2>
+        <label for="usuario">Usuário</label>
+        <input type="text" id="usuario" placeholder="Digite seu usuário">
+        <label for="senha">Senha</label>
+        <input type="password" id="senha" placeholder="Digite sua senha">
+        <a href="esqueceuSenha.html">Esqueceu a senha?</a>
+        <button>Login</button>
+    </div>
+</body>
+</html>
