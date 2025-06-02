d8a2ea831461b1d4be8dc757edb775072fc261f6
Pamella <pamellarafeli@gmail.com>
2025-06-02 16:47:53 -0300

    Delete Arquivos/Banco de Dados/teste.sql

--- Arquivos/Banco de Dados/teste.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE jogos;
-USE jogos;
-
-CREATE TABLE historico_jogos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome_jogo VARCHAR(100) NOT NULL,
-    hora_entrada DATETIME NOT NULL
-);
